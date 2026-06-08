8fd71065c5c81eb273a147754cb24065d23bfc27
eyedeekay <hankhill19580@gmail.com>
2026-06-07 20:01:19 -0400

    Add ident command to I2P control client and update documentation

---
+++ lib/control.go
@@ -0,0 +1,270 @@
+// Package lib provides a clean, reusable API for interacting with the I2P control interface.
+package lib
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-i2p/go-i2pcontrol"
+)
+
+// Config holds configuration for the I2P control client.
+type Config struct {
+	Host     string
+	Port     string
+	Path     string
+	Password string
+	Verbose  bool
+}
+
+// Client represents a connection to the I2P control interface.
+type Client struct {
+	config Config
+}
+
+// CommandOptions holds options for command execution.
+type CommandOptions struct {
+	Block bool
+	Args  []string
+}
+
+// CommandResult represents the result of a command execution.
+type CommandResult struct {
+	Success      bool
+	Output       string
+	Error        error
+	IsShutdown   bool
+	WaitShutdown bool
+}
+
+// New creates a new I2P control client with the given configuration.
+func New(config Config) *Client {
+	return &Client{
+		config: config,
+	}
+}
+
+// Connect initializes the connection to the I2P control interface.
+func (c *Client) Connect() error {
+	i2pcontrol.Initialize(c.config.Host, c.config.Port, c.config.Path)
+	_, err := i2pcontrol.Authenticate(c.config.Password)
+	return err
+}
+
+// logVerbose logs a message if verbose mode is enabled.
+func (c *Client) logVerbose(msg string) {
+	if c.config.Verbose {
+		fmt.Println(msg)
+	}
+}
+
+// Execute runs a command and returns the result.
+func (c *Client) Execute(command string, opts CommandOptions) CommandResult {
+	result := CommandResult{
+		Success:      true,
+		IsShutdown:   false,
+		WaitShutdown: false,
+	}
+
+	c.logVerbose(command)
+
+	switch command {
+	case "echo":
+		message, err := i2pcontrol.Echo(strings.Join(opts.Args, " "))
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = message
+
+	case "ident":
+		message, err := i2pcontrol.RouterHash()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = message
+
+	case "restart":
+		message, err := i2pcontrol.Restart()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = message
+		result.IsShutdown = true
+		result.WaitShutdown = opts.Block
+
+	case "graceful-restart":
+		message, err := i2pcontrol.RestartGraceful()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = message
+		result.IsShutdown = true
+		result.WaitShutdown = opts.Block
+
+	case "shutdown":
+		message, err := i2pcontrol.Shutdown()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = message
+		result.IsShutdown = true
+		result.WaitShutdown = opts.Block
+
+	case "graceful-shutdown":
+		message, err := i2pcontrol.ShutdownGraceful()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = message
+		result.IsShutdown = true
+		result.WaitShutdown = opts.Block
+
+	case "update":
+		needsUpdate, err := i2pcontrol.FindUpdates()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		if needsUpdate {
+			result.Output = "You need an update"
+			message, err := i2pcontrol.Update()
+			if err != nil {
+				result.Success = false
+				result.Error = err
+				break
+			}
+			result.Output = message
+		} else {
+			result.Output = "You don't need an update"
+		}
+
+	case "find-update":
+		needsUpdate, err := i2pcontrol.FindUpdates()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		if needsUpdate {
+			result.Output = "You need an update"
+		} else {
+			result.Output = "You don't need an update"
+		}
+
+	case "stat":
+		message, err := i2pcontrol.Status()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = fmt.Sprintf("%v", message)
+
+	case "netstat":
+		message, err := i2pcontrol.NetStatus()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = fmt.Sprintf("%v", message)
+
+	case "reseedstat":
+		isReseeding, err := i2pcontrol.Reseeding()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		if isReseeding {
+			result.Output = "Router is reseeding"
+		} else {
+			result.Output = "Router is not reseeding"
+		}
+
+	case "tunstat":
+		message, err := i2pcontrol.ParticipatingTunnels()
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = fmt.Sprintf("%v", message)
+
+	case "ratestat":
+		if len(opts.Args) < 2 {
+			result.Success = false
+			result.Error = fmt.Errorf("ratestat requires 2 arguments")
+			break
+		}
+		interval, err := strconv.Atoi(opts.Args[1])
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		message, err := i2pcontrol.RateStat(opts.Args[0], interval)
+		if err != nil {
+			result.Success = false
+			result.Error = err
+			break
+		}
+		result.Output = fmt.Sprintf("%v", message)
+
+	default:
+		result.Success = false
+		result.Error = fmt.Errorf("unknown command: %s", command)
+	}
+
+	return result
+}
+
+// WaitForShutdown blocks until the router is shut down.
+// Returns the number of tunnels that were participating at shutdown.
+func (c *Client) WaitForShutdown() (int, error) {
+	lastParticipatingTunnels, err := i2pcontrol.ParticipatingTunnels()
+	if err != nil {
+		return 0, err
+	}
+
+	baseminutes := time.Duration(time.Minute * 11)
+	if lastParticipatingTunnels != 0 {
+		fmt.Printf("Waiting for expiration of %d participating tunnels in %v\n", lastParticipatingTunnels, baseminutes)
+	}
+
+	oldtime := time.Now()
+	for {
+		participatingTunnels, err := i2pcontrol.ParticipatingTunnels()
+		if err != nil {
+			return 0, err
+		}
+
+		if participatingTunnels != lastParticipatingTunnels {
+			minutes := oldtime.Sub(time.Now())
+			fmt.Printf("Waiting for expiration of %d participating tunnels in %v\n", participatingTunnels, baseminutes+minutes)
+			lastParticipatingTunnels = participatingTunnels
+		}
+
+		time.Sleep(time.Duration(time.Second * 1))
+		if participatingTunnels < 1 {
+			break
+		}
+	}
+
+	return lastParticipatingTunnels, nil
+}
--- main.go
+++ main.go
@@ -4,11 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"strconv"
-	"strings"
-	"time"
 
-	"github.com/go-i2p/go-i2pcontrol"
+	"github.com/go-i2p/i2p-control/lib"
 )
 
 var usage = `i2p-control
@@ -41,6 +38,7 @@ The methods that have been implemented are
         graceful-shutdown : i2pcontrol:ShutdownGraceful
         update            : i2pcontrol:Update
         find-update       : i2pcontrol:FindUpdate
+		ident             : i2pcontrol:RouterInfo:hash(go-i2p only)
 
 So, for instance, to initiate a graceful shutdown and block until the router is
 shut down, use the command:
@@ -62,164 +60,52 @@ var (
 	block    = flag.Bool("block", false, "Block the terminal until the router is completely shut down")
 )
 
-var args = flag.Args()
-
 func main() {
 	flag.Parse()
 	if *shelp || *lhelp {
 		fmt.Printf(usage)
 		return
 	}
+
+	args := flag.Args()
 	if len(args) < 2 {
 		args = append(args, "bw.sendBps")
 		args = append(args, "300000")
 	}
-	shuttingdown := false
-	i2pcontrol.Initialize(*host, *port, *path)
-	_, err := i2pcontrol.Authenticate(*password)
-	if err != nil {
+
+	// Create client configuration
+	config := lib.Config{
+		Host:     *host,
+		Port:     *port,
+		Path:     *path,
+		Password: *password,
+		Verbose:  *sverbose || *lverbose,
+	}
+
+	// Create and connect client
+	client := lib.New(config)
+	if err := client.Connect(); err != nil {
 		log.Fatal(err)
 	}
-	verbose := false
-	if *sverbose {
-		verbose = true
+
+	// Execute command
+	result := client.Execute(*command, lib.CommandOptions{
+		Block: *block,
+		Args:  args,
+	})
+
+	if !result.Success {
+		log.Fatal(result.Error)
 	}
-	if *lverbose {
-		verbose = true
+
+	if result.Output != "" {
+		fmt.Println(result.Output)
 	}
-	if verbose {
-		log.Println(*command)
-	}
-	minutes := time.Duration(time.Minute * 11)
-	baseminutes := time.Duration(time.Minute * 11)
-	oldtime := time.Now()
-	switch *command {
-	case "echo":
-		message, err := i2pcontrol.Echo(strings.Join(flag.Args(), " "))
-		if err != nil {
+
+	// Handle shutdown if needed
+	if result.IsShutdown && result.WaitShutdown {
+		if _, err := client.WaitForShutdown(); err != nil {
 			log.Fatal(err)
-		}
-		log.Println(message)
-	case "ident":
-		message, err := i2pcontrol.RouterHash()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(message)
-	case "restart":
-		message, err := i2pcontrol.Restart()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(message)
-		shuttingdown = true
-	case "graceful-restart":
-		message, err := i2pcontrol.RestartGraceful()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(message)
-		shuttingdown = true
-	case "shutdown":
-		message, err := i2pcontrol.Shutdown()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(message)
-		shuttingdown = true
-	case "graceful-shutdown":
-		message, err := i2pcontrol.ShutdownGraceful()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(message)
-		shuttingdown = true
-	case "update":
-		message, err := i2pcontrol.FindUpdates()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if message {
-			log.Println("You need an update")
-			message, err := i2pcontrol.Update()
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println(message)
-		}
-		log.Println("You don't need an update")
-	case "find-update":
-		message, err := i2pcontrol.FindUpdates()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if message {
-			log.Println("You need an update")
-			return
-		}
-		log.Println("You don't need an update")
-	case "stat":
-		message, err := i2pcontrol.Status()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(message)
-	case "netstat":
-		message, err := i2pcontrol.NetStatus()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(message)
-	case "reseedstat":
-		message, err := i2pcontrol.Reseeding()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if message {
-			log.Println("Router is reseeding")
-		} else {
-			log.Println("Router is not reseeding")
-		}
-	case "tunstat":
-		message, err := i2pcontrol.ParticipatingTunnels()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(message)
-	case "ratestat":
-		args2, err := strconv.Atoi(args[1])
-		if err != nil {
-			log.Fatal(err, args2)
-		}
-		message, err := i2pcontrol.RateStat(args[0], args2)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(message)
-	}
-	if shuttingdown {
-		lastParticipatingTunnels, err := i2pcontrol.ParticipatingTunnels()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if lastParticipatingTunnels != 0 {
-			log.Println("Waiting for expiration of:", lastParticipatingTunnels, "participating tunnels in", baseminutes)
-		}
-		for *block {
-			minutes = oldtime.Sub(time.Now())
-			participatingTunnels, err := i2pcontrol.ParticipatingTunnels()
-			if err != nil {
-				log.Fatal(err)
-			}
-			if participatingTunnels != lastParticipatingTunnels {
-				log.Println("Waiting for expiration of:", participatingTunnels, "participating tunnels in", baseminutes+minutes)
-				lastParticipatingTunnels = participatingTunnels
-			}
-			time.Sleep(time.Duration(time.Second * 1))
-			if participatingTunnels < 1 {
-				*block = false
-				break
-			}
 		}
 	}
 }
